c103ced91e64a489b6239e2b52a6d9d34b8099be
clintcparker@gmail.com <clintcparker@gmail.com>
2013-08-22 16:33:35 -0700

    added solution

---
+++ FakesTesting/BadIdea.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Helpers;
+
+namespace FakesTesting
+{
+    public class BadIdea
+    {
+        /// <summary>
+        /// public wrapper, returns part of the object
+        /// </summary>
+        /// <returns></returns>
+        public string GetNewBadIdea()
+        {
+            try
+            {
+                var json = GetBadIdea();
+                return json.badidea;
+            }
+            catch (Exception)
+            {
+                return "Uh Oh. We just had an accident.";
+            }
+        }
+
+
+        /// <summary>
+        /// private method to handle network
+        /// </summary>
+        /// <returns></returns>
+        private dynamic GetBadIdea(bool weirdBool = false)
+        {
+            if (!weirdBool){
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = httpClient.GetAsync("http://badideas.herokuapp.com/").Result;
+                response.EnsureSuccessStatusCode(); 
+                var responseContent = response.Content;
+                var responseBody = responseContent.ReadAsStringAsync().Result;
+                var json = Json.Decode(responseBody);
+                return json;
+            }
+            dynamic d = new
+                {
+                    badidea = "It's like soylent for foodies"
+                };
+            return d;
+        }
+    }
+}
---
+++ UnitTests/BadIdeaTests.cs
@@ -0,0 +1,96 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Fakes;
+using System.Threading.Tasks;
+using FakesTesting;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BadIdeaTests
+    {
+        [TestInitialize]
+        public void SetupTests()
+        {
+
+        }
+
+        [TestMethod]
+        public void BadIdeaUnitTest()
+        {
+            using (ShimsContext.Create())
+            {
+                SetupBadIdeaHttpShims();
+
+                var badIdeaClass = new BadIdea();
+                var result = badIdeaClass.GetNewBadIdea();
+                Assert.IsInstanceOfType(result, typeof (string));
+            }
+        }
+
+        [TestMethod]
+        public void PrivateGetBadIdeaTest()
+        {
+            using (ShimsContext.Create())
+            {
+                SetupBadIdeaHttpShims();
+
+                //REMEMBER: DON'T FORGET TO SET THE INTERNALS VISIBLE ATTRIBUTE
+                var privateBadIdea = new PrivateObject(new BadIdea());
+
+                //PARADOX: OPTIONAL PARAMETERS ARE REQUIRED
+                dynamic privateResult = privateBadIdea.Invoke("GetBadIdea", false);
+
+                Assert.IsInstanceOfType(privateResult.badidea, typeof (string));
+            }
+        }
+
+        [TestMethod]
+        public void PrivateGetBadIdeaWithParamTest()
+        {
+            using (ShimsContext.Create())
+            {
+                SetupBadIdeaHttpShims();
+
+                var privateBadIdea = new PrivateObject(new BadIdea());
+
+                dynamic privateResult = privateBadIdea.Invoke("GetBadIdea", true);
+
+                Assert.AreEqual(privateResult.badidea, "It's like soylent for foodies");
+            }
+        }
+
+        [TestMethod]
+        public void GetBadIdeaExceptionTest()
+        {
+            using (ShimsContext.Create())
+            {
+                ShimHttpClient.AllInstances.GetAsyncString = (client, s) =>
+                    {
+                        var httpResponseMessage = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                        var task = Task.FromResult(httpResponseMessage);
+                        return task;
+                    };
+
+                var badIdeaClass = new BadIdea();
+                var result = badIdeaClass.GetNewBadIdea();
+                Assert.IsInstanceOfType(result, typeof(string));
+            }
+        }
+
+        private static void SetupBadIdeaHttpShims()
+        {
+            ShimHttpClient.AllInstances.GetAsyncString = (client, s) =>
+            {
+                var httpResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("{badidea:\"It's like tumblr for lamps\"}")
+                };
+                var task = Task.FromResult(httpResponseMessage);
+                return task;
+            };
+        }
+    }
+}
